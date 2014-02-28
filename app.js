2debb27a4febe62d65055772da33c6d3a08157e8
Blackening999 <pointhomefinal@gmail.com>
2014-02-28 17:17:39 +0200

    Init

---
+++ app.js
@@ -0,0 +1,66 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var config = require('config');
+var log = require('libs/log')(module);
+
+var app = express();
+app.engine('ejs', require('ejs-locals'));
+app.set('views', path.join(__dirname, 'templates'));
+app.set('view engine', 'ejs');
+
+app.use(express.favicon());
+if (app.get('env') == 'development') {
+	app.use(express.logger('dev'));
+} else {
+	app.use(express.logger('default'));
+}
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.use(express.cookieParser());
+
+app.use(app.router);
+
+app.get('/', function(req, res, next) {
+	res.render("index", {
+		body: '<b>Helllo!!!</b>'
+	})
+})
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res) {
+	res.send(404, "Page not found");
+});
+
+app.use(function(err, req, res, next) {
+	if (app.get('env') == 'development') {
+		var errorHandler = express.errorHandler();
+		errorHandler(err, req, res, next);
+	} else {
+		res.send(500);
+	}
+});
+
+
+
+//var routes = require('./routes');
+//var user = require('./routes/user');
+//
+//
+//// all environments
+
+
+//// development only
+//if ('development' == app.get('env')) {
+//  app.use(express.errorHandler());
+//}
+//
+//app.get('/', routes.index);
+//app.get('/users', user.list);
+//
+http.createServer(app).listen(config.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
