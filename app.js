46faec263fb34caec9c57c70e991f50a7db8a1a5
Blackening999 <pointhomefinal@gmail.com>
2014-03-04 23:08:04 +0200

    Preparations to deploy 4

--- app.js
+++ app.js
@@ -63,7 +63,8 @@ app.use(function(err, req, res, next) {
 });
 
 //chat here
-var server = http.createServer(app).listen(config.get('port'), function() {
+var port = Number(process.env.PORT || 5000);
+var server = http.createServer(app).listen(port, function() {
 	console.log('Express server listening on port ' + config.get('port'));
 });
 
