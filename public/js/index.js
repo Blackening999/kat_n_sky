d30f80e3703dcd0e920f8edcccab5ae53f6685ee
Blackening999 <pointhomefinal@gmail.com>
2014-03-07 11:42:09 +0200

    Front-end smart text decoration

--- public/js/index.js
+++ public/js/index.js
@@ -4,12 +4,12 @@
 $(document).ready(function() {
 	var navPanel = $('.nav.main-navigation');
 
-	navPanel.find("signIn").off("click").on("click", function(e) {
+	navPanel.find("#signIn").off("click").on("click", function(e) {
 		e.preventDefault();
 		location.href = "/login";
 	});
 
-	navPanel.find("signOut").off("click").on("click", function(e) {
+	navPanel.find("#signOut").off("click").on("click", function(e) {
 		e.preventDefault();
 		$.post("/logout").done(function() {
 			return;
