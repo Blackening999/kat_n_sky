d0883489911c54e3b9a5fb5949e29c629ee3af08
Blackening999 <pointhomefinal@gmail.com>
2014-03-08 12:29:12 +0200

    More sounds

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
 	playAlert.content['main'] = 'http://www.mediacollege.com/downloads/sound-effects/star-wars/darthvader/darthvader_powerofthedarkside.wav';
-	if (location.href.indexOf("/") < 1) playAlert('main');
+	playAlert.content['login'] = 'http://www.mediacollege.com/downloads/sound-effects/star-wars/darthvader/darthvader_dontmakeme.wav';
+	playAlert.content['logout'] = 'http://www.mediacollege.com/downloads/sound-effects/star-wars/darthvader/darthvader_lackoffaith.wav';
+	if (location.href.indexOf("/login") > 1) {
+		playAlert('login')
+	} else if (location.href.indexOf("/logout") > 1){
+		playAlert('logout');
+	} else if (location.href.indexOf("/chat") > 1) {
+	} else {
+		playAlert('main');
+	}
 	var navPanel = $('.nav.main-navigation');
 	navPanel.find("#signIn").off("click").on("click", function(e) {
 		e.preventDefault();
