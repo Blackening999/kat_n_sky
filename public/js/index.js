c4ced0a9fb912a48a38e4ed929c6e6be7a7d00ee
Blackening999 <pointhomefinal@gmail.com>
2014-03-08 12:22:13 +0200

    Super interference

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	playAlert.content['main'] = 'http://www.mediacollege.com/downloads/sound-effects/star-wars/darthvader/darthvader_powerofthedarkside.wav';
-	playAlert('main');
+	if (location.href.indexOf("/") < 1) playAlert('main');
 	var navPanel = $('.nav.main-navigation');
 	navPanel.find("#signIn").off("click").on("click", function(e) {
 		e.preventDefault();
